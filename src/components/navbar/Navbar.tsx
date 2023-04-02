fe01a027d0132eda48c65c3e69fb3ab3b6adeeac
Vinicius <vinicius.diniz444@gmail.com>
2023-04-02 20:57:35 -0300

    responsive navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,17 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
 import "./navbar.scss";
 
 const Navbar: React.FC = () => {
+  const [open, setOpen] = React.useState<boolean>(false);
+
+  const toggleMenu = () => {
+    if(window.innerWidth < 769){
+      setOpen(!open)
+    }
+  }
+
+  const menuStyle = open ? "menu open" : "menu"
+
   return (
     <div className="navbar">
       <div className="brand">Teste</div>
       <div className="hamburguer">
-        <MenuIcon />
+        <MenuIcon onClick={toggleMenu}/>
       </div>
-      <div className="menu">
+      <div className={menuStyle}>
         <ul>
+          <CloseIcon className="close" onClick={toggleMenu}/>
           <li>
             <Link to="/">Home</Link>
           </li>
